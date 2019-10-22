8aea4ce13764773f951e24ab116828c39494992e
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2019-10-22 16:02:53 -0300

    First commit

---
+++ mvc/Controllers/TestController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using mvc.Models;
+
+
+namespace mvc.Controllers
+{
+    public class TestController : Controller
+    {
+        //
+        // GET: /Test/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string mensaje()
+        {
+            return "ejemplo de mensaje simple";
+        }
+
+        public ActionResult mensaje2()
+        {
+            return Content("ejemplo content");
+        }
+
+        public ActionResult mensaje1()
+        {
+            return View("vista");
+        }
+
+        public ActionResult libro1()
+        {
+            Libro book = new Libro();
+            book.codigo = 10;
+            book.nombre = "asp mvc";
+            book.precio = 1000;
+            ViewData["MiLibro"] = book;
+            return View();
+        }
+
+        public ActionResult libro2()
+        {
+            Libro book = new Libro();
+            book.codigo = 9;
+            book.nombre = "java";
+            book.precio = 1500;
+            return View(book);
+        }
+
+        public ActionResult Agregar()
+        {
+            return View("View1");
+        }
+	}
+}
---
+++ mvc/Models/Libro.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mvc.Models
+{
+    public class Libro
+    {
+        public int codigo { get ; set; }
+        public string nombre { get ; set; }
+        public int precio { get; set; }
+    }
+}
